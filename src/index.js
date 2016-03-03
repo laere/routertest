6cf27b57143c582e381af8cf3783aca582795a9b
Zack Bostian <bostianz@gmail.com>
2016-03-03 08:22:25 -0500

    seperated navbar into diff folder

--- src/index.js
+++ src/index.js
@@ -1,27 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, combineReducers } from 'redux';
+// MIDDLEWARE
 import Thunk from 'redux-thunk';
 import Promise from 'redux-promise-middleware';
-import { Router, Route, browserHistory, IndexRoute } from 'react-router';
+// ROUTING
+import { Router, Route, browserHistory } from 'react-router';
 import { syncHistoryWithStore, routerReducer } from 'react-router-redux';
-
-
+// COMPONENTS
 import App from './components/app';
 import Page1 from './components/Page1';
 import Page2 from './components/Page2';
+// REDUCERS
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware(Thunk, Promise)(createStore);
 
+const createStoreWithMiddleware = applyMiddleware(Thunk, Promise)(createStore);
 // Store with middleware and reducers
 const State = combineReducers({
   routing: routerReducer
 });
 
 const store = createStoreWithMiddleware(State);
-
 // Sync history with store
 const history = syncHistoryWithStore(browserHistory, store);
 
